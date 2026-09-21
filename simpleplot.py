d18034a6f87d3960802eb2002aa31bb4ac52c417
skarim-hbs <158502738+skarim-hbs@users.noreply.github.com>
2026-09-21 11:11:40 -0400

    Create simpleplot.py

---
+++ simpleplot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.title("Sine Wave")
+plt.grid(True)
+plt.show()
